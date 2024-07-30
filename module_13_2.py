b80a4648223763f33f2290ca0bac9df9b7c7635a
123Lex123 <78158427+123Lex123@users.noreply.github.com>
2024-07-30 12:23:30 +0300

    Create module_13_2.py

---
+++ module_13_2.py
@@ -0,0 +1,24 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+# Вставьте ваш токен сюда
+TOKEN = 'TOKEN'
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.reply("Привет! Я бот помогающий твоему здоровью.")
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.reply("Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
